ba6fae243f6cf9f743c5ba41cb7647524b9a4e04
Syed Ridwanuddin <syedridwan0301@gmail.com>
2025-01-01 16:52:45 +0530

    remove .env file

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 const sessionOptions={
-  secret:"mysupersecretcode",
+  secret:"process.env.SECRET",
   resave:false,
   saveUninitialized:true,
   cookie:{
